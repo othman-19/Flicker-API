8c41d47896a76580ae33f425fe4c4b753d62162f
othmane <othman-19@hotmail.fr>
2019-11-15 22:41:13 +0100

    improve home page and using flikr methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,8 @@
 class StaticPagesController < ApplicationController
-  include StaticPagesHelper
-
   def home
-    if params[:user_id]
-  		@user_id = params[:user_id]
-  		#here call method that you will store in helper
-  		#to search for photos
-  	end	
+    return unless params[:user_id]
+      @user_id = params[:user_id]
+      @user = flickr.people.findByUsername username: params[:username]
+      @photostream = flickr.people.getPublicPhotos user_id: @user['id']
   end
 end
