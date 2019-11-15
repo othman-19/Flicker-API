59dc9ab31ac8355faa2d45f0e2ac8f9db2eba1c2
othmane <othman-19@hotmail.fr>
2019-11-15 21:53:10 +0100

    add search form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+  include StaticPagesHelper
+
   def home
+    if params[:user_id]
+  		@user_id = params[:user_id]
+  		#here call method that you will store in helper
+  		#to search for photos
+  	end	
   end
 end
