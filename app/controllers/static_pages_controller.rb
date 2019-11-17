81ee171959d8c7f58a9d79751e217bb25af53a6a
stickler-ci <support@stickler-ci.com>
2019-11-17 20:55:29 +0000

    Fixing style errors.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class StaticPagesController < ApplicationController
   def home
-    @flickr_id = params[:flickr_id] || "185516062@N03"
-    flickr = FlickRaw::Flickr.new #(ENV['FLICKRAW_API_KEY'], ENV['FLICKRAW_SHARED_SECRET'])
+    @flickr_id = params[:flickr_id] || '185516062@N03'
+    flickr = FlickRaw::Flickr.new # (ENV['FLICKRAW_API_KEY'], ENV['FLICKRAW_SHARED_SECRET'])
     @photos = flickr.people.getPublicPhotos(user_id: @flickr_id, per_page: 50)
   end
 end
