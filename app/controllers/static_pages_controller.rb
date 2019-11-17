5cc8149a556f78b64ca0f02d76c0f17a623ecc41
othmane <othman-19@hotmail.fr>
2019-11-17 21:34:55 +0100

    fix Flikr API bugs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    return unless params[:user_id]
-      @user_id = params[:user_id]
-      @user = flickr.people.findByUsername username: params[:username]
-      @photostream = flickr.people.getPublicPhotos user_id: @user['id']
+    @flickr_id = params[:flickr_id] || "185516062@N03"
+    flickr = FlickRaw::Flickr.new #(ENV['FLICKRAW_API_KEY'], ENV['FLICKRAW_SHARED_SECRET'])
+    @photos = flickr.people.getPublicPhotos(user_id: @flickr_id, per_page: 50)
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'flickraw'
+
+#FlickRaw.api_key="e65c93e43a854cfca0a4b25194f96fdc"
+#FlickRaw.shared_secret="32b58fee4b293cb9"
+FlickRaw.api_key=ENV["flickr_api_key"]
+FlickRaw.shared_secret=ENV["flickr_api_secret"]
